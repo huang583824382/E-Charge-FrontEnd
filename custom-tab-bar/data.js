921c70979b06f4f36d47934d355c6f89ae4bc29d
huang583824382 <3190103047@zju.edu.cn>
2022-04-22 00:48:42 +0800

    Init

---
+++ custom-tab-bar/data.js
@@ -0,0 +1,16 @@
+export default [{
+    icon: 'home',
+    text: '广场',
+    url: 'pages/home/home',
+  },
+  {
+    icon: 'cart',
+    text: '发布',
+    url: 'publish',
+  },
+  {
+    icon: 'person',
+    text: '自己',
+    url: 'pages/usercenter/index',
+  },
+];
